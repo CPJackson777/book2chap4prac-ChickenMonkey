6ba1782f6f4defd457b4dcd647cfc8f75e7c2ddd
Charles Jackson <cpjackson572@gmail.com>
2020-04-25 22:44:18 -0500

    added more concise solution idea in comments

--- main.js
+++ main.js
@@ -5,7 +5,7 @@
 
 
 for (let currentNumber = 1; currentNumber <= 100; currentNumber++) {
-    if (currentNumber % 5 === 0 && currentNumber % 7 === 0) {      
+    if (currentNumber % 5 === 0 && currentNumber % 7 === 0) {      //could also just say if(currentNumber % 35 === 0)
         console.log('ChickenMonkey')
     } else if (currentNumber % 5 === 0) {
         console.log("Chicken");
